5ac08c416aa6cad2a63a589a4ff8dc941932d0df
Dhruba Chakraborty <dhruba.chakraborty.2000@gmail.com>
2024-08-13 12:54:30 +0600

    feat: added new lighting

--- src/main.js
+++ src/main.js
@@ -1,19 +1,13 @@
 import * as THREE from 'three';
 import { VRButton } from 'three/examples/jsm/webxr/VRButton.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { RectAreaLightHelper } from 'three/examples/jsm/Addons.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
-const panoSphereGeo = new THREE.SphereGeometry(6, 16, 16);
 
-// Create the panoramic sphere material
-const panoSphereMat = new THREE.MeshStandardMaterial({
-	side: THREE.BackSide,
-	displacementScale: - 4.0
-});
-// THREE.ColorManagement.enabled = false
-// Create the panoramic sphere mesh
-sphere = new THREE.Mesh(panoSphereGeo, panoSphereMat);
+THREE.ColorManagement.enabled = false
+console.log(THREE.REVISION)
+
+
 init()
 animate()
 
@@ -23,55 +17,30 @@ function init() {
 
     // Set up the scene
     scene = new THREE.Scene();
-    // Load a 360° image as the scene background
-    // const sceneLoader = new THREE.TextureLoader();
-    // const sceneTexture = sceneLoader.load('/src/assets/nature.jpg', () => {
-    //     console.log("Scene loaded");
-    //     const rt = new THREE.WebGLCubeRenderTarget(sceneTexture.image.height);
-    //     rt.fromEquirectangularTexture(renderer, sceneTexture);
-    //     scene.background = rt.sceneTexture;
-    // });
-
-    // Load and assign the texture and depth map
-	let manager = new THREE.LoadingManager();
-	let imageloader = new THREE.TextureLoader(manager);
-	//change the image to A
-	imageloader.load('/src/assets/nature.jpg', function (texture) {
-		texture.colorSpace = THREE.SRGBColorSpace;
-		texture.minFilter = THREE.NearestFilter;
-		texture.generateMipmaps = false;
-		texture.anisotropy = 16
-        texture.encoding = THREE.LinearEncoding;
-		sphere.material.map = texture;
-    });
-
-	// On load complete add the panoramic sphere to the scene
-	manager.onLoad = function () {
-		sphere.scale.set(30, 20, 50)
-		sphere.rotation.set(0, -Math.PI / 2, 0)
-		sphere.position.z = 0
-        sphere.position.y = 65
-		sphere.position.x = 0.9
-		scene.add(sphere);
-
-	};
+    scene.background = new THREE.Color(0x000000);
 
     // Set up the camera
     camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.01, 1000);
-    // camera.position.set(0, 18, 40);
-    camera.position.set(80,9,48)
+    camera.position.set(0, 18, 40);
 
-    // Add ambient light
-    const light_ambient = new THREE.AmbientLight(0xffffff, 0.8  );
+    const light_ambient = new THREE.AmbientLight(0xff8877, 0.5*Math.PI);
     scene.add(light_ambient);
   
-    var light_directional_left = new THREE.DirectionalLight(0xff5522, 3)
-    light_directional_left.position.set(-1 , 0, 10)
+    light_directional_left = new THREE.DirectionalLight(0xffccdd, 1.2*Math.PI)
+    light_directional_left.position.set(-1 , 0, 0.7)
     scene.add(light_directional_left);
   
-    var light_directional_right = new THREE.DirectionalLight(0x99aaff, 3)
-    light_directional_right.position.set(6, 2, -2)
+    // Add helper
+    var directionalLightHelper = new THREE.DirectionalLightHelper(light_directional_left);
+    scene.add(directionalLightHelper);
+  
+    light_directional_right = new THREE.DirectionalLight(0xccaaff, 1.2*Math.PI)
+    light_directional_right.position.set(2, 1, 0.7)
     scene.add(light_directional_right);
+  
+     // Add helper
+     var directionalLightHelper2 = new THREE.DirectionalLightHelper(light_directional_right);
+    scene.add(directionalLightHelper2);
   
     
 
@@ -92,14 +61,7 @@ function init() {
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
-    // renderer.outputColorSpace = THREE.sRGBEncoding;
-    // renderer.outputEncoding = THREE.LinearEncoding; // Disable sRGB encoding
-    // renderer.toneMapping = THREE.NoToneMapping;    // Disable tone mapping
-
-    // renderer.gammaFactor = 2.2;  // Adjust gamma factor
-    // renderer.gammaOutput = true; // Enable gamma output
-
-
+    renderer.outputEncoding = THREE.sRGBEncoding;
     renderer.xr.enabled = true;
     document.body.appendChild(renderer.domElement);
 
