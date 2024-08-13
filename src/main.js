59de1f356a5e46c71fa57402ce30310078cb2bb4
mdshakilkhan841 <mdshakilkhan841@gmail.com>
2024-08-13 12:54:44 +0600

    fix color and lighting issue

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import { VRButton } from 'three/examples/jsm/webxr/VRButton.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader, RectAreaLightHelper } from 'three/examples/jsm/Addons.js';
 
-
+THREE.ColorManagement.enabled = false;
+console.log(THREE.REVISION)
 init();
 animate();
 
@@ -18,16 +19,23 @@ function init() {
     camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.01, 1000);
     camera.position.set(0, 18, 40);
 
-    const light_ambient = new THREE.AmbientLight(0xffffff, 1);
+
+    const light_ambient = new THREE.AmbientLight(0xff6688, 0.4 * Math.PI);
     scene.add(light_ambient);
 
-    var light_directional_left = new THREE.DirectionalLight(0x99aaff, 1.6)
+    var light_directional_left = new THREE.DirectionalLight(0x99aaff, 1.6 * Math.PI)
     light_directional_left.position.set(-1, 0, 1)
     scene.add(light_directional_left);
 
-    var light_directional_right = new THREE.DirectionalLight(0xff5522, 2)
+    const helper1 = new THREE.DirectionalLightHelper(light_directional_left);
+    scene.add(helper1);
+
+    var light_directional_right = new THREE.DirectionalLight(0xff5522, 2 * Math.PI)
     light_directional_right.position.set(2, -1, 1)
     scene.add(light_directional_right);
+
+    const helper2 = new THREE.DirectionalLightHelper(light_directional_right);
+    scene.add(helper2);
 
     // LOAD A MAP TO SWAP ---->. TYPE "noggin.material.map= nogginMap2" IN CONSOLE
     texture = new THREE.TextureLoader().load("./maps/girlMapDotNose.jpg");
